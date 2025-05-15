33825dc8b1d01d3c9e7ea6639726ccb8c0689673
vivek shimpi <your.vivekshimpi146@gmail.com.com>
2025-05-15 19:40:36 +0530

    change remove

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,14 +2,10 @@ import admin from 'firebase-admin';
 
 const serviceAccountStr = process.env.FIREBASE_SERVICE_ACCOUNT;
 
-// Replace the placeholder @ with real newline \n before parsing
-const serviceAccount = JSON.parse(
-  serviceAccountStr.replace(/@/g, '\n')
-);
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccountStr),
   });
 }
 
