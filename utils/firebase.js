4280a6537e1975231ce783e944c9cf473fdf3ae6
vivek shimpi <your.vivekshimpi146@gmail.com.com>
2025-05-16 10:48:57 +0530

    change

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,21 +1,7 @@
-<<<<<<< HEAD
 const base64ServiceAccount = process.env.FIREBASE_SERVICE_ACCOUNTS;
-=======
-import admin from 'firebase-admin';
-
-const serviceAccountStr = process.env.FIREBASE_SERVICE_ACCOUNT;
-
-if (!serviceAccountStr) {
-  throw new Error("❌ FIREBASE_SERVICE_ACCOUNT env variable is missing.");
-}
-
-const serviceAccount = JSON.parse(
-  serviceAccountStr.replace(/@/g, '\n') // replace all @ with newline
-);
->>>>>>> 4dcdf5c4f1edfe4a8c888c3787de87dbba97bc4d
 
 if (!base64ServiceAccount) {
-  throw new Error("❌ FIREBASE_SERVICE_ACCOUNT is missing.");
+  throw new Error("❌ FIREBASE_SERVICE_ACCOUNTS is missing.");
 }
 
 const serviceAccount = JSON.parse(Buffer.from(base64ServiceAccount, 'base64').toString('utf-8'));
